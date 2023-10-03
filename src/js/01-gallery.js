79c3c1049b9dae07890a4e1954614b0231ab1fa5
DmitrFr <122161314+DmitrFr@users.noreply.github.com>
2023-10-03 02:54:06 +0200

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const makeListElementMarkup = function ({ preview, original, description }) {
+  return `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>
+`;
+};
+
+const makeGalleryList = galleryItems.map(makeListElementMarkup).join('');
+const listEl = document.querySelector('.gallery');
+listEl.innerHTML = makeGalleryList;
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('form');
+const localStorageKeyName = 'feedback-form-state';
+const formData = {};
+
+const onInputText = function (evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(localStorageKeyName, JSON.stringify(formData));
+};
+
+const onFormSubmit = function (evt) {
+  evt.preventDefault();
+  evt.target.reset();
+  localStorage.removeItem(localStorageKeyName);
+};
+
+const populateInputs = function () {
+  const savedMessage = JSON.parse(localStorage.getItem(localStorageKeyName));
+  console.log(savedMessage);
+  if (savedMessage) {
+    formEl.elements.email.value = savedMessage.email;
+    formEl.elements.message.value = savedMessage.message;
+  }
+};
+
+formEl.addEventListener('input', throttle(onInputText, 500));
+formEl.addEventListener('submit', throttle(onFormSubmit, 500));
+populateInputs();
+
+//stringify objektu , a pri perezagruzke sdelat parce
+//pri pustom forma pokazyvaet null
